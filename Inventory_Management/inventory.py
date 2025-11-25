eb423ad893cd1fde7d5e261d9cc663d9c32a902b
Tommy Ghaly <tjghaly07@gmail.com>
2025-11-25 14:10:31 +0200

    Implement search and filter methods in InventoryService class

--- Inventory_Management/inventory.py
+++ Inventory_Management/inventory.py
@@ -1,2 +1,14 @@
 class InventoryService():
     pass
+
+
+        
+        
+    def search_by_name():
+        pass
+    
+    def filter_by_price():
+        pass
+    
+    def filter_by_category():
+        pass
