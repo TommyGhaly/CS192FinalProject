51588f381cdb67a5b37912df61d9c1c216f7429a
Tommy Ghaly <tjghaly07@gmail.com>
2025-12-11 00:07:14 +0200

    Enhance Order and OrderService classes with detailed docstrings for methods; improve initialization, order creation, stock updating, and order retrieval functionalities for better clarity and documentation.

--- Shopping/Orders/order.py
+++ Shopping/Orders/order.py
@@ -1,6 +1,29 @@
 from typing import *
 class Order():
+    """
+    Order recipt class
+    """
+    
     def __init__(self, order_id:str, customer_id:str, items:Dict, total_price: float, status:str, payment_info: Dict):
+        """
+        Initialization of the Order class
+        
+        Args:
+            order_id (str): Order ID
+            customer_id (str): Customer's ID
+            item (dict): Dictionary containing each product and their quantity in the order
+            total_price (float): cost of each item in the items dictionary
+            status (str): string to describe the nature of the order 
+            payment_info (dict): dictionary describing the payemnt used 
+        
+        Attributes:
+            order_id: order ID
+            customer_id: customer's ID
+            items: products mapped to quantity
+            total_price: total cost of all items
+            _status: nature of order
+            _payment: info for payment 
+        """
         self.order_id = order_id
         self.customer_id = customer_id
         self.items = items 
@@ -8,11 +31,27 @@ class Order():
         self._status = status
         self._payment = payment_info
     
+    
     @property
     def payment(self) -> Dict:
+        """
+        Property method to retrieve the payment info
+        
+        Returns:
+            dict: payment info
+        """
+        
         return self._payment
     
+    
     def to_dict(self) -> Dict:
+        """
+        Method to convert Order object into a dictionary
+        
+        Returns:
+            dict: order object saved as a dictionary
+        """
+        
         return {
             'order_id': self.order_id,
             'customer_id': self.customer_id,
@@ -22,8 +61,19 @@ class Order():
             'payment': self._payment
         } 
     
+    
     @classmethod
     def from_dict(cls, data:dict) -> 'Order':
+        """
+        Class method to convert a dictionary into an Order object
+        
+        Args:
+            data (dict): dictionary describing order object
+        
+        Returns:
+            Order: order described by inputted dictionary
+        """
+        
         return cls(
             order_id=data['order_id'],
             customer_id=data['customer_id'],
--- Shopping/Orders/orderService.py
+++ Shopping/Orders/orderService.py
@@ -9,13 +9,38 @@ import json
 import logging
 
 class OrderService():
+    """
+    Order Service object to facilitate orders
+    """
+    
     def __init__(self):
+        """
+        Initialization for order service object
+        
+        Attributes:
+            order_data: dictionary of all orders loaded from JSON file
+            pm: product managment object
+            inventory_service: inventory service object
+            dm: data management object
+        """
+        
         self.order_data = OrderService.load_orders()
         self.pm = ProductManagement()
         self.inventory_service = InventoryService()
         self.dm = DataManagement()
         
+        
     def create_order(self, cart: Cart, payment: Payment, payment_info: dict, order_id: str) -> Order:
+        """
+        Method to create and process orders
+        
+        Args:
+            cart (Cart): Cart object containing all of the items in the order
+            payment (Payment): Payment object showing how payment went through
+            payment_info (dict): all the contents of the payment 
+            order_id (str): order's ID
+        """
+        
         reserved_stock = {}
         items = cart.get_items()
         total_price = cart.get_total(self.pm)
@@ -23,25 +48,28 @@ class OrderService():
         
         for key, value in items.items():
             
-            if self.inventory_service.check_stock(key) > value:
+            if self.inventory_service.check_stock(key) > value: # ensure enough stock
                 
                 reserved_stock[key] = value
                 logging.log(f'Reserving {value} of {key} stock')
+                
             else:
                 
                 logging.warning(f'Not enough stock to process order. Releasing reserved stock')
-                order = Order(order_id, customer_id, items, total_price, 'failed', payment_info)
+                order = Order(order_id, customer_id, items, total_price, 'failed', payment_info) # create order object that fails
                 self.order_data[order_id] = order.to_dict()
                 self.dm.save_order_data(self.order_data)
+                OrderService.save_orders(self.order_data)
                 return order
         
                                 
-        if payment.process_payment(total_price, payment_info):
+        if payment.process_payment(total_price, payment_info): # ensure payment processes correctly
             self.update_stocks(reserved_stock)
             logging.log(f'Order paid successfully!')
             order = Order(order_id, customer_id, cart.get_items(), total_price, 'paid', payment_info)
             self.order_data[order_id] = order.to_dict
             self.dm.save_order_data(self.order_data)
+            OrderService.save_orders(self.order_data)
             return order
         
         else:
@@ -49,27 +77,62 @@ class OrderService():
             order = Order(order_id, customer_id, items, total_price, 'failed', payment_info)
             self.order_data[order_id] = order.to_dict()
             self.dm.save_order_data(self.order_data)
+            OrderService.save_orders(self.order_data)
             return order
         
         
     def update_stocks(self, reserved_stock: Dict[str:int]):
+        """
+        Update the inventory and product list 
+        
+        Args:
+            reserved_stock (Dict[str:int]): dictionary mapping each product bought with a quantity to update
+                                            files
+        """
+        
         for key, value in reserved_stock.items():
             self.pm.remove_product(key, value)
     
     
     def get_order(self, order_id: str) -> Order:
+        """
+        Retrieve order from the order id
+        
+        Args:
+            order_id (str): order's identifier
+        
+        Returns:
+            Order: Order correlating to order_id
+        """
+        
         return Order.from_dict(self.order_data[order_id])
     
-    def list_orders_for_customer(self, customer_id: str) -> List[Order]:
+    
+    def list_orders_for_customer(self, customer_id: str) -> List[Dict]:
+        """
+        Get list of Orders done by a certain customer
+        
+        Args:
+            customer_id (str): customer's ID
+        
+        Returns:
+            List[Order]: list of each order in a dictionary
+        """
+        
         orders = []
         for _, value in self.order_data:
             if value.get('customer_id', '') == customer_id:
-                orders.append(Order.from_dict(value))
+                orders.append(value)
         
         return orders
     
+    
     @staticmethod
     def load_orders():
+        """
+        Static method to load the orders from the JSON file
+        """
+        
         try: 
             with open('orders.json', 'r') as f:
                 orders_data = json.load(f)
@@ -77,7 +140,15 @@ class OrderService():
         except FileNotFoundError:
             return {}
 
+
     @staticmethod
     def save_orders(orders_data: Dict[str, Dict]):
+        """
+        Static method to save the orders to JSON file
+        
+        Args:
+            order_data (dict[str, dict]): order data in the form of dictionaries        
+        """
+        
         with open('orders.json', 'w') as f:
             json.dump(orders_data, f)  
