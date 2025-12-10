2bf8d85ef9d7faa4fcabe08d51a8db228164cdda
Tommy Ghaly <tjghaly07@gmail.com>
2025-12-10 16:29:11 +0200

    Implement order creation and stock management in OrderService; enhance payment processing and order retrieval methods for improved functionality.

--- Shopping/Orders/order.py
+++ Shopping/Orders/order.py
@@ -1,22 +1,25 @@
 from typing import *
 class Order():
-    def __init__(self, order_id:str, customer_id:str, items:List[Dict], total_price: float, status:str, created_at:str):
+    def __init__(self, order_id:str, customer_id:str, items:Dict, total_price: float, status:str, payment_info: Dict):
         self.order_id = order_id
         self.customer_id = customer_id
-        self.items = items # List of dicts with product_id and quantity
+        self.items = items 
         self.total_price = total_price
         self._status = status
-        self.created_at = created_at
-
+        self._payment = payment_info
     
-    def to_dict(self) -> dict:
+    @property
+    def payment(self) -> Dict:
+        return self._payment
+    
+    def to_dict(self) -> Dict:
         return {
-            "order_id": self.order_id,
-            "customer_id": self.customer_id,
-            "items": self.items,
-            "total_price": self.total_price,
-            "status": self._status,
-            "created_at": self.created_at
+            'order_id': self.order_id,
+            'customer_id': self.customer_id,
+            'items': self.items,
+            'total_price': self.total_price,
+            'status': self._status,
+            'payment': self._payment
         } 
     
     @classmethod
@@ -27,5 +30,4 @@ class Order():
             items=data['items'],
             total_price=data['total_price'],
             status=data['status'],
-            created_at=data['created_at']
         )
--- Shopping/Orders/orderService.py
+++ Shopping/Orders/orderService.py
@@ -1,20 +1,73 @@
 from .order import Order
 from ..Carts.cart import Cart
 from ..Payments.payment import Payment
+from ...Products.productManagement import ProductManagement
+from ...Inventory_Management.inventory import InventoryService
+from ...Data_Persistence.data import DataManagement
 from typing import *
 import json 
+import logging
 
 class OrderService():
     def __init__(self):
         self.order_data = OrderService.load_orders()
-    def create_order(self, cart: Cart, payment: Payment, payment_info: dict) -> Order:
-        pass
+        self.pm = ProductManagement()
+        self.inventory_service = InventoryService()
+        self.dm = DataManagement()
+        
+    def create_order(self, cart: Cart, payment: Payment, payment_info: dict, order_id: str) -> Order:
+        reserved_stock = {}
+        items = cart.get_items()
+        total_price = cart.get_total(self.pm)
+        customer_id = cart.customer_id
+        
+        for key, value in items.items():
+            
+            if self.inventory_service.check_stock(key) > value:
+                
+                reserved_stock[key] = value
+                logging.log(f'Reserving {value} of {key} stock')
+            else:
+                
+                logging.warning(f'Not enough stock to process order. Releasing reserved stock')
+                order = Order(order_id, customer_id, items, total_price, 'failed', payment_info)
+                self.order_data[order_id] = order.to_dict()
+                self.dm.save_order_data(self.order_data)
+                return order
+        
+                                
+        if payment.process_payment(total_price, payment_info):
+            self.update_stocks(reserved_stock)
+            logging.log(f'Order paid successfully!')
+            order = Order(order_id, customer_id, cart.get_items(), total_price, 'paid', payment_info)
+            self.order_data[order_id] = order.to_dict
+            self.dm.save_order_data(self.order_data)
+            return order
+        
+        else:
+            logging.warning(f'Failed to process payment!')
+            order = Order(order_id, customer_id, items, total_price, 'failed', payment_info)
+            self.order_data[order_id] = order.to_dict()
+            self.dm.save_order_data(self.order_data)
+            return order
+        
+        
+    def update_stocks(self, reserved_stock: Dict[str:int]):
+        for key, value in reserved_stock.items():
+            for i in range(value):
+                self.pm.remove_product(key)
+    
     
     def get_order(self, order_id: str) -> Order:
-        pass
+        return Order.from_dict(self.order_data[order_id])
     
     def list_orders_for_customer(self, customer_id: str) -> List[Order]:
-        pass
+        orders = []
+        for _, value in self.order_data:
+            if value.get('customer_id', '') == customer_id:
+                orders.append(Order.from_dict(value))
+        
+        return orders
     
     @staticmethod
     def load_orders():
